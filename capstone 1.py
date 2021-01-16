ae8fd8655d0a6a033fe98dda81a58a96e57b52d1
Minakh1991 <76989000+Minakh1991@users.noreply.github.com>
2021-01-16 14:07:13 -0800

    Add files via upload

---
+++ capstone 1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # this notebook will be mainly used for the capstone project
+
+# In[3]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
